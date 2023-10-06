ff3614bad051053e536ebcf23fe5c90d6f8530ac
My Name <myEmail@example.com>
2023-10-06 04:39:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static bool SearchBin(int[] mass, int poisk)
+    {
+        int left = 0;
+        int right = mass.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (mass[mid] == poisk)
+                return true;
+            else if (mass[mid] < poisk)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 5;
+        bool found = SearchBin(nums, target);
+        Console.WriteLine(found);
+    }
+}
